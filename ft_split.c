0edd691fc5d0db4dd1d4d674019af9ea62c12466
John Habaguhirwa <jhabaguh@F3C023.42adel.org.au>
2021-09-16 12:45:00 +0930

    finished all of them, norm left

---
+++ ft_split.c
@@ -0,0 +1,57 @@
+static char	word_after(char const *s, char c)
+{
+	while(*s && *s == c)
+		s++;
+	return((char *)s);
+}
+
+static size_t	word_len(char const *s, char c)
+{
+	size_t = i;
+
+	i = 0;
+	while(s[i] && s[i] != c)
+		i++;
+	return(i)
+}
+
+void *clean_current(char **split, size_t current)
+{
+	size_t = i;
+
+	i = 0;
+	while(i < current)
+	{
+		free(split[i]);
+		i++;
+	}
+	free(split);
+	return (NULL);
+}
+
+char	**ft_split(char const *s, char c)
+{
+	char 	**split;
+	size_t	current;
+	size_t	wordcount;
+
+	wordcount = ft_wordsets((char *)s, c);
+	split = (char**)malloc(sizeof(char*) * (wordcount + 1));
+	if (split == NULL)
+		return(NULL);
+	current = 0;
+	while (current < wordcount)
+	{
+		s = word_after(s, c);
+		split[current] = ft_strsub(s, 0, word_len(s, c));
+		if (split[current] == NULL)
+		{
+			clean_current(split, current);
+			return(NULL);
+		}
+		current++;
+		s = s + word_len(s, c);
+	}
+	split(wordcount) = NULL;
+	return(split);
+}
---
+++ ft_wordsets.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+size_t	ft_wordsets(char *str, char sep)
+{
+	size_t = sets;
+	size_t i;
+
+	while(str[i])
+	{
+		while(str[i] && str[i] == sep)
+			i++;
+		while(str[i] && str[i] != sep)
+			i++
+		result++;
+	}
+	return(result);
+}
